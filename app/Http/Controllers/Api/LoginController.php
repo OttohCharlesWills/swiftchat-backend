72f1c863cae5cfb583d62dab13dc64ded38e4032
ottohcharleswills <ottohcharleswills@gmail.com>
2026-09-16 10:42:48 +0100

    change of service

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
-use App\Services\SmsService;
+use App\Services\EmailVerificationService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
