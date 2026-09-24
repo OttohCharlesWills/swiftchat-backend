e48cc1bd3e41d48f0a58161280f3520a473b555d
ottohcharleswills <ottohcharleswills@gmail.com>
2026-09-24 08:02:20 +0100

    update

--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Broadcast;
 
 Route::get('/app-version', function () {
     return response()->json([
-        'version' => '1.0.3',
-        'version_code' => 2,
+        'version' => '1.0.0',
+        'version_code' => 3,
         'download_url' => 'https://coremonitor.in/downloads/swiftchat-1.0.0+3.apk',
         'message' => 'Please update to access new features.',
     ]);
